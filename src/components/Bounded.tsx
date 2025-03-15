4c892a4e6c5ba8c1a4551c87246c466b30bb6403
Tanisha Jayabalan <tanisha.jayabalan24@gmail.com>
2025-03-15 16:30:30 +0530

    bugs

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,26 +1,26 @@
-import React from "react"
-import clsx from "clsx"
-type BoundedProps = 
-{
-    as?: React.ElementType;
+import React from "react";
+import clsx from "clsx";
+
+type BoundedProps<T extends React.ElementType> = {
+    as?: T;
     className?: string;
-    children: React.ReactNode;
+    children?: React.ReactNode; // Allow children explicitly
+} & React.ComponentPropsWithoutRef<T>; // Add props specific to the `as` component
 
-};
+const Bounded = React.forwardRef<
+    HTMLElement,
+    BoundedProps<any> // Make the props generic
+>(({ as: Comp = "section", className, children, ...restProps }, ref) => {
+    return (
+        <Comp
+            ref={ref}
+            className={clsx("px-4 py-10 md:px-6 md:py-14 lg:py-16", className)}
+            {...restProps}
+        >
+            <div className="mx-auto w-full max-w-7xl">{children}</div>
+        </Comp>
+    );
+});
 
-const Bounded = React.forwardRef<HTMLDListElement, BoundedProps>(
-    ({as: Comp = "section", className, children, ...restProps},ref)=>
-    {
-        return (
-            <Comp ref={ref} className={clsx('px-4 py-10 md:px-6 md:py-14 lg:py-16',className)}>
-            <div className="mx-auto w-full max-w-7xl">
-                {children}
-            </div>
-            </Comp>
-        )
-    
-    }
-)
-
-Bounded.displayName="Bounded"
-export default Bounded
+Bounded.displayName = "Bounded";
+export default Bounded;
