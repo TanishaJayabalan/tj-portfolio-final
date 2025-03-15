55d6190d9f6f86a042bc14150366d75c64f98d70
Tanisha Jayabalan <145171678+TanishaJayabalan@users.noreply.github.com>
2025-03-15 21:12:31 +0530

    eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "warn" // Add this line to downgrade the any error
+    }
+  }
 ];
 
 export default eslintConfig;
